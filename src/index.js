954df41b0731b1ace54426d4e39f57aefa50b19d
Sergey Stupak <itstupak@gmail.com>
2024-11-23 21:49:23 +0100

    server+initMongoDB+sessions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // запуск сервера, подключение к бд
+
+import { initMongoDB } from "./db/initMongoDB.js"
+import { setupServer } from "./server.js";
+
+const bootstrap = async () => {
+  await initMongoDB();
+  setupServer();
+};
+
+void bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,1 +1,27 @@
 //server
+
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '8080'));
+
+export function setupServer() {
+
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
