a5e27e26b827b77453851599dddae23231544159
Decembric <dmitro15121979@gmail.com>
2024-11-24 21:25:26 +0200

    Created water branch

--- src/db/models/watersSchema.js
+++ src/db/models/watersSchema.js
@@ -1,1 +1,46 @@
 //water schema
+// import { number } from 'joi';
+import mongoose, { model, Schema } from 'mongoose';
+import Joi from 'joi';
+
+const waterSchema = new Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    date: { type: String, required: true },
+    currentDailyNorm: { type: Number, required: true },
+    createdAt: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const createValidationSchema = Joi.object({
+  amount: Joi.number().required(),
+  date: Joi.string()
+    .pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)
+    .required(),
+  currentDailyNorm: Joi.number().required(),
+  createdAt: Joi.string()
+    .pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)
+    .required(),
+});
+
+export const updateValidationSchema = Joi.object({
+  amount: Joi.number().required(),
+  date: Joi.string()
+    .pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)
+    .required(),
+  currentDailyNorm: Joi.number().required(),
+});
+
+export const WaterModel = model('water', waterSchema);
--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
 //server
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandler';
+
+export const setupServer = () => {
+  const app = express();
+  const PORT = 8080;
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.use(cors());
+  app.use(express.json());
+  app.get('/', (req, res) => {
+    res.json({ message: 'water' });
+  });
+
+  app.use('/api/water');
+  app.use(errorHandler);
+  app.listen(PORT, () => {
+    console.log('Connect was successfully');
+  });
+};
