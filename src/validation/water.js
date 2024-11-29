31a060fe85e371b9e57a585a8417f2a536f3bf88
Decembric <dmitro15121979@gmail.com>
2024-11-29 22:12:30 +0200

    Added date pattern

--- src/validation/water.js
+++ src/validation/water.js
@@ -3,9 +3,9 @@ import Joi from 'joi';
 
 export const createValidationSchema = Joi.object({
   amount: Joi.number().required(),
-  // date: Joi.string()
-  //   .pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)
-  //   .required(),
+  date: Joi.string()
+    .pattern(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)
+    .required(),
   currentDailyNorm: Joi.number().required(),
 });
 
