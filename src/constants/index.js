1a34a900ecf2ffc821c6136f9782057a1ad4a347
Oleksii Ozyrskyi <aozyrskyii@gmail.com>
2024-12-01 12:37:42 +0100

    check

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,15 +6,16 @@ export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
 export const GENDER = {
-    WOMAN: 'woman',
-    MAN: 'man',
+  WOMAN: 'woman',
+  MAN: 'man',
 };
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 export const CLOUDINARY = {
-    CLOUD_NAME: 'CLOUD_NAME',
-    API_KEY: 'API_KEY',
-    API_SECRET: 'API_SECRET',
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
 };
 
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
