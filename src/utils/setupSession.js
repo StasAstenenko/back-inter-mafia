16b9bb9724264c1020dfba67f6ca94ff22f0c83a
StasAstenenko <107870729+StasAstenenko@users.noreply.github.com>
2024-12-05 15:41:47 +0200

    secure cookie

--- src/utils/setupSession.js
+++ src/utils/setupSession.js
@@ -1,12 +1,14 @@
-import { THIRTY_DAYS } from "../constants/index.js";
+import { THIRTY_DAYS } from '../constants/index.js';
 
 export const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
+    secure: true,
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
   res.cookie('sessionId', session._id, {
     httpOnly: true,
+    secure: true,
     expires: new Date(Date.now() + THIRTY_DAYS),
   });
 };
