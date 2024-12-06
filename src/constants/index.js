ba191301f3256f6ce4ac0fa830c3097b087de235
Igor Yevtushenko <miller.bertram777@gmail.com>
2024-12-06 10:12:53 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
 
 import path from 'node:path';
 
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const FIFTEEN_MINUTES = 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
 export const GENDER = {
